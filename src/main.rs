a4f4efe66c9f0975445794526c982a95190473ea
Justin Kasteleijn <justin.kasteleijn@gmail.com>
2024-12-02 13:25:09 +0100

    :)

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,190 @@
-fn main() {
-    let mut actor1 = ActorActress{ name: String::from("BradPitt"), movies: vec![]};
-    let mut actor2 = ActorActress{ name: String::from("NormanReedus"), movies: vec![]};
-    let mut actress1 = ActorActress{ name: String::from("DianaKruger"), movies: vec![]};
-    let mut actress2 = ActorActress{ name: String::from("MelanieLaurent"), movies: vec![]};
-    let women = vec![actress1.name.clone(), actress2.name.clone()];
-    let men = vec![actor1.name.clone(), actor2.name.clone()];
-    let mov1: Movies = Movies{ name: String::from("ingloriousBasterd"), actor_actress: vec![actress1.name.clone(), actress2.name.clone(), actor1.name.clone()]};
-    let mov2: Movies = Movies{ name: String::from("Sky"), actor_actress: vec![actress1.name.clone(), actor2.name.clone()]};
-    actor1.movies.push(mov1.name.clone());
-    actor2.movies.push(mov2.name.clone());
-    actress1.movies.push(mov1.name.clone());
-    actress1.movies.push(mov2.name.clone());
-    actress2.movies.push(mov1.name.clone());
-    let allmovies: Vec<Movies> = vec![mov1, mov2];
+use std::cmp::PartialEq;
+use std::collections::HashSet;
+use std::fmt::{Debug, Display};
+use std::hash::Hash;
+use std::io::{stdin, BufRead, BufReader};
+use std::str::{FromStr, SplitWhitespace};
 
-    game(allmovies.clone(), women, men,"Veronique", allmovies);
+struct Input<B> {
+    inner: B,
+    buffer: String,
 }
 
-#[derive(Clone)]
-struct ActorActress{
-    name: String,
-    movies: Vec<String>
-}
-
-#[derive(Clone)]
-struct Movies{
-    name: String,
-    actor_actress: Vec<String>
-}
-
-fn game(movies: Vec<Movies>, actrices: Vec<String>, actors: Vec<String>, turn: &str, all_movies: Vec<Movies>){
-    if actrices.len() == 0 && turn == "Veronique" {
-        println!("{}", "Mark");
-    }else if actors.len() == 0 && turn == "Mark" {
-        println!("{}", "Veronique");
-    }else{
-        if turn == "Mark" {
-            new_movie(movies, actors, actrices, turn, all_movies);
-        }else{
-            new_movie(movies, actors, actrices, turn, all_movies);
+impl<B: BufRead> Input<B> {
+    pub fn new(inner: B) -> Input<B> {
+        Self {
+            inner,
+            buffer: String::new(),
         }
+    }
+
+    pub fn line(&mut self) -> Line {
+        self.buffer.clear();
+        self.inner.read_line(&mut self.buffer).unwrap();
+        Line {
+            split: self.buffer.split_whitespace(),
+        }
+    }
+
+    pub fn string(&mut self) -> String {
+        self.buffer.clear();
+        self.inner.read_line(&mut self.buffer).unwrap();
+        self.buffer.to_string()
+    }
+
+    fn parse<T: FromStr>(&mut self) -> T
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        self.string().trim().parse::<T>().unwrap()
     }
 }
 
-fn new_movie(movies: Vec<Movies>, possible_actors: Vec<String>, possible_actrices: Vec<String>, turn: &str, all_movies: Vec<Movies>){
+struct Line<'a> {
+    split: SplitWhitespace<'a>,
+}
+
+impl<'a> Line<'a> {
+    fn next<T: FromStr>(&mut self) -> T
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        self
+            .split
+            .next()
+            .unwrap()
+            .parse::<T>()
+            .unwrap()
+    }
+    fn pair<T: FromStr>(&mut self) -> (T, T)
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        (self.next(), self.next())
+    }
+    fn collect<T: FromStr>(self) -> Vec<T>
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        self
+            .split
+            .map(|d| d.parse::<T>().unwrap())
+            .collect::<Vec<T>>()
+    }
+}
+#[derive(Clone)]
+struct Actors {
+    name: String,
+    movies: HashSet<String>,
+}
+
+#[derive(Clone)]
+struct Movies {
+    title: String,
+    cast: HashSet<String>,
+}
+
+#[derive(Eq, PartialEq)]
+enum Player {
+    Veronique,
+    Mark,
+}
+
+fn main() {
+    let input = stdin();
+    let mut input = Input::new(BufReader::new(input.lock()));
+
+    let (n, m) = input.line().pair::<usize>();
+
+    let mut actresses = (0..n)
+        .map(|_| input.string())
+        .collect::<Vec<String>>();
+
+    let mut actors = (0..n)
+        .map(|_| input.string())
+        .collect::<Vec<String>>();
+
+    let mut movie_casts: Vec<Movies> = Vec::with_capacity(m);
+
+    for _ in 0..m {
+        let movie_title: String = input.string();
+        let cast_size: usize = input.parse::<usize>();
+        let cast: HashSet<String> = (0..cast_size).map(|_| input.string()).collect::<HashSet<String>>();
+
+        movie_casts.push(
+            Movies {
+                title: movie_title,
+                cast,
+            });
+    }
+
+    game(&movie_casts, &actresses, &actors, Player::Veronique);
+}
+
+fn game(movies: &Vec<Movies>, actresses: &Vec<String>, actors: &Vec<String>, player: Player, turn: &str) -> Option<Player> {
+    if actresses.len() == 0 && player == Player::Veronique {
+        Some(Player::Mark)
+    } else if actors.len() == 0 && player == Player::Mark {
+        Some(Player::Veronique)
+    } else if player == Player::Mark {
+        new_new_move(movies, actors, actresses, player, turn)
+    } else {
+        new_new_move(movies, actors, actresses, player, turn)
+    }
+}
+
+fn new_new_move(movies: &Vec<Movies>, possible_actors: &Vec<String>, possible_actresses: &Vec<String>, player: Player, turn: &str) -> Option<Player> {
+    if player == Player::Veronique {
+        for y in possible_actresses {
+            let c = movies
+                .iter()
+                .filter_map(|m| {
+                    if m.cast.contains(y) && m.cast.contains(turn) {
+                        let n =  possible_actresses.clone();
+                        n.iter().enumerate().find(|(c, acc)| )
+                        game(movies,)
+                    }
+                })
+        }
+    } else {
+
+    }
+    None
+}
+
+fn new_movie(movies: &Vec<Movies>, possible_actors: &Vec<String>, possible_actresses: &Vec<String>, turn: Player, all_movies: &Vec<Movies>) {
     let mut _index: usize = 0;
     let mut result: Vec<Movies> = vec![];
     for movie in movies {
-        for actor in movie.actor_actress{
-            if turn == "Mark"{
-                for possible in possible_actors.iter(){
+        for actor in movie.cast {
+            if turn == Player::Mark {
+                for possible in possible_actors.iter() {
                     if &actor == possible {
                         possible_actors.clone().remove(_index);
-                        for m in all_movies.iter(){
-                            if m.actor_actress.contains(&actor) {
+                        for m in all_movies.iter() {
+                            if m.cast.contains(&actor) {
                                 result.push(m.clone());
                             }
                         }
-                        game(result.clone(), possible_actrices.clone(), possible_actors.clone(), "Veronique", all_movies.clone());
+                        game(result, possible_actresses.clone(), possible_actors.clone(), "Veronique", all_movies.clone());
                         break;
                     }
                     _index = _index + 1;
                 }
-            }
-            else if turn == "Veronique"{
-                for possible in possible_actrices.iter(){
+            } else if turn == "Veronique" {
+                for possible in possible_actresses.iter() {
                     if &actor == possible {
-                        possible_actrices.clone().remove(_index);
-                        for m in all_movies.iter(){
-                            if m.actor_actress.contains(&actor) {
+                        possible_actresses.clone().remove(_index);
+                        for m in all_movies.iter() {
+                            if m.cast.contains(&actor) {
                                 result.push(m.clone());
                             }
                         }
-                        game(result.clone(), possible_actrices.clone(), possible_actors.clone(), "Mark", all_movies.clone());
+                        game(result.clone(), possible_actresses.clone(), possible_actors.clone(), "Mark", all_movies.clone());
                         break;
                     }
                     _index = _index + 1;
